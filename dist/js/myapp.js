5b2396a58bf68570f75acded06a08a8b649657cf
rajeshpatra <p7.rajesh@gmail.com>
2016-09-04 05:51:04 +0530

    final touchdown

--- dist/js/myapp.js
+++ dist/js/myapp.js
@@ -1,23 +1,53 @@
-var main = function() {
-	$('.icon-menu').click(function() {
-		$('.menu').animate({
-			left: '0px';
-		}, 200);
+// var main = function() {
+// 	$('.icon-menu').click(function() {
+// 		$('.menu').animate({
+// 			left: '0px';
+// 		}, 200);
 
-		$('body').animate({
-			left: '285px'
-		}, 200);
-	});
+// 		$('body').animate({
+// 			left: '285px'
+// 		}, 200);
+// 	});
 
-	$('.icon-close').click(function() {
-		$('.menu').animate({
-			left: '-285px'
-		}, 200);
+// 	$('.icon-close').click(function() {
+// 		$('.menu').animate({
+// 			left: '-285px'
+// 		}, 200);
 
-		$('body').animate({
-			left: '0px'
-		}, 200);
-	});
-};
+// 		$('body').animate({
+// 			left: '0px'
+// 		}, 200);
+// 	});
+// };
 
-$(document).ready(main);
+
+// var main = function() {
+// 	$('.account').click(function() {
+// 		var x = $(this).attr('id');
+// 		if(x == 1) {
+// 			$('.submenu').hide();
+// 			$(this).attr('id', 0);
+// 		} else {
+// 			$('.submenu').show)();
+// 			$(this).attr('id' 1);
+// 		}
+// 	});
+// };
+
+
+// $(document).ready(main);
+
+
+
+// <!-- toggle nav bar -->
+
+// var main = function() {
+//   $('.dropdown-toggle').click(function(event) {
+//   	// $('.dropdown-menu').hide)();
+//   	$('.root').toggle();
+    
+//   });
+// }
+
+// $(document).ready(main);
+
